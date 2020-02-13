ad7956fd030245dd7a95e42800e51cd071afe534
Gergely Bekesi <gergely.bekesi@bitrise.io>
2020-02-13 08:34:11 +0100

    Add router and mod tidy

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/bitrise-io/addons-test-backend
 
 go 1.13
+
+require (
+	github.com/DataDog/datadog-go v3.4.0+incompatible // indirect
+	github.com/aws/aws-sdk-go v1.29.1 // indirect
+	github.com/bitrise-io/api-utils v0.0.0-20191108154647-23f446e98177
+	github.com/c2fo/testify v0.0.0-20150827203832-fba96363964a
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/jinzhu/gorm v1.9.12
+	github.com/justinas/alice v1.2.0
+	github.com/philhofer/fwd v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/rs/cors v1.7.0 // indirect
+	github.com/tinylib/msgp v1.1.1 // indirect
+	go.uber.org/zap v1.13.0
+	gopkg.in/DataDog/dd-trace-go.v1 v1.20.1
+)
---
+++ router/router.go
@@ -0,0 +1,36 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/bitrise-io/addons-test-backend/actions"
+	"github.com/bitrise-io/addons-test-backend/env"
+	"github.com/bitrise-io/addons-test-backend/middlewares.go"
+	"github.com/bitrise-io/api-utils/handlers"
+	"github.com/justinas/alice"
+	"gopkg.in/DataDog/dd-trace-go.v1/contrib/gorilla/mux"
+)
+
+// New ...
+func New(appEnv *env.AppEnv) *mux.Router {
+	// StrictSlash: allow "trim slash"; /x/ REDIRECTS to /x
+	r := mux.NewRouter(mux.WithServiceName("addons-test-mux")).StrictSlash(true)
+
+	for _, route := range []struct {
+		path           string
+		middleware     alice.Chain
+		handler        func(e *env.AppEnv, w http.ResponseWriter, r *http.Request) error
+		allowedMethods []string
+	}{
+		{
+			path: "/", middleware: middlewares.CommonMiddleware(appEnv),
+			handler: actions.RootHandler, allowedMethods: []string{"GET", "OPTIONS"},
+		},
+	} {
+		r.Handle(route.path, route.middleware.Then(actions.Handler{Env: appEnv, H: route.handler})).
+			Methods(route.allowedMethods...)
+	}
+
+	r.NotFoundHandler = middlewares.CommonMiddleware(appEnv).Then(&handlers.NotFoundHandler{})
+	return r
+}
