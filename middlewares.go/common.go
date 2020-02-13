65c9a9c90ce6fdd76bef323f425a4be251575e54
Gergely Bekesi <gergely.bekesi@bitrise.io>
2020-02-13 08:33:30 +0100

    Add common middleware

---
+++ middlewares.go/common.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import (
+	"github.com/bitrise-io/addons-test-backend/env"
+	"github.com/bitrise-io/api-utils/middleware"
+	"github.com/justinas/alice"
+)
+
+// CommonMiddleware ...
+func CommonMiddleware(appEnv *env.AppEnv) alice.Chain {
+	baseMiddleware := middleware.CommonMiddleware()
+
+	if appEnv.Environment == env.ServerEnvProduction {
+		baseMiddleware = baseMiddleware.Append(
+			middleware.CreateRedirectToHTTPSMiddleware(),
+		)
+	}
+	return baseMiddleware.Append(middleware.CreateOptionsRequestTerminatorMiddleware())
+}
