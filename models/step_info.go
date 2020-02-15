3175134b077764a66480c29b4b8bcf43570fd75f
Gergely Bekesi <gergely.bekesi@bitrise.io>
2020-02-15 18:50:14 +0100

    Add step info models

---
+++ models/step_info.go
@@ -0,0 +1,9 @@
+package models
+
+// StepInfo ...
+type StepInfo struct {
+	ID      string `json:"id"`
+	Version string `json:"version"`
+	Title   string `json:"title"`
+	Number  int    `json:"number"`
+}
